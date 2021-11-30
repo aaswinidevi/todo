47fc89c886827848aad5ff6b5e6dcae669e39780
aswini devi <aaswinidevi@gmail.com>
2021-11-30 08:58:45 +0530

    first commit

---
+++ list.js
@@ -0,0 +1,28 @@
+var validate = (callback) => {
+  var user = document.getElementById("user").value;
+  var pass = document.getElementById("password").value;
+  $("#user").text(" ");
+  $("#pass").text(" ");
+  var count = 0;
+
+  if (user.trim() === "admin" && pass.trim() === "12345") {
+    localStorage.setItem("username", "admin");
+    callback();
+  } else if (user.trim() !== "" && pass.trim() !== "") {
+    $("#pass").text("Wrong Username or Password ");
+    $("#pass").css("color", "red");
+  }
+  if (user.trim() === "") {
+    $("#user").text("Username cannot be blank");
+    $("#user").css("color", "red");
+  }
+  if (pass.trim() === "") {
+    $("#pass").text("Password cannot be blank");
+    $("#pass").css("color", "red");
+  }
+};
+
+function redirect() {
+  location.href = "list.html";
+}
+
